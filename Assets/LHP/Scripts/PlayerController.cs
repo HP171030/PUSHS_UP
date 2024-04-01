7f27b702ae8008e5c53955cc78c985410f6fb2a5
HyeongPil Lee <hexagongame@naver.com>
2024-04-01 14:09:21 +0900

    feat : Pull,Push 구현
    상단에 놓인 오브젝트도 같이 움직이도록 구현

--- Assets/LHP/Scripts/PlayerController.cs
+++ Assets/LHP/Scripts/PlayerController.cs
@@ -1,23 +1,26 @@
-    using System.Collections;
-    using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.EventSystems;
-    using UnityEngine.InputSystem;
-using UnityEngine.Windows;
+using UnityEngine.InputSystem;
+using Cinemachine;
+using System.Collections.Generic;
 
-    public class PlayerController : MonoBehaviour
-    {
-      private Vector3 moveDir;
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 moveDir;
 
-        bool moveOn;
+    bool moveOn;
     bool grabOn;
     [Header("Player")]
-        [SerializeField] float moveDistance;
+    [SerializeField] float moveDistance;
     [SerializeField] float moveSpeed;
 
     [Header("Configs")]
     [SerializeField] Animator animator;
-      [SerializeField] Rigidbody rb;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] CinemachineVirtualCamera cam1;
+    [SerializeField] CinemachineVirtualCamera cam2;
+    [SerializeField] bool OnPlayer1On;
+
 
     Transform obstacle;
     RaycastHit otherObs;
@@ -27,106 +30,106 @@ using UnityEngine.Windows;
     {
         OffPull();
         MoveFunc();
-        
+
     }
 
     private void Start()
-        {
-            rb = GetComponent<Rigidbody>();
-            animator = GetComponent<Animator>();
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
 
-        }
-        private void OnMove(InputValue value)
-        {
-        
-            Vector2 input = value.Get<Vector2>();
-            moveDir = new Vector3(input.x, 0, input.y);
+    }
+    private void OnMove( InputValue value )
+    {
 
-        }
+        Vector2 input = value.Get<Vector2>();
+        moveDir = new Vector3(input.x, 0, input.y);
 
-    public void OnPull(InputValue value)
+    }
+
+    public void OnPull( InputValue value )
     {
         if ( value.isPressed )
         {
             grabOn = true;
             Debug.Log("잡기");
             animator.SetTrigger("PullStart");
-           
+
             if ( Physics.Raycast(transform.position, transform.forward, out grabHit, 1f) )
             {
                 if ( grabHit.collider.gameObject.CompareTag("Obstacle") )
                 {
                     animator.SetBool("Pull", true);
-                   Debug.Log("잡음");
-                    
+                    Debug.Log("잡음");
+
                 }
-                   
+
             }
         }
-        else 
+        else
         {
             grabOn = false;
             Debug.Log("놓기");
 
 
         }
-       
+
     }
     public void OffPull()
     {
-        if ( !grabOn && !moveOn)
+        if ( !grabOn && !moveOn )
         {
             animator.SetBool("Pull", false);
         }
-        
+
     }
     public void MoveFunc()
     {
         if ( !moveOn && !grabOn )
         {
-        animator.SetFloat("MoveSpeed", moveDir.magnitude);
-        if ( Mathf.Abs(moveDir.x) != 0 && Mathf.Abs(moveDir.z) != 0 )
-        {
-           
-            return;
-        }
-        else if ( !moveOn && moveDir.magnitude > 0 )
-        {
-            moveOn = true;
-            
-            StartCoroutine(MoveRoutine(moveDir));
-            
-            transform.forward = moveDir;
-           
-        }
+            animator.SetFloat("MoveSpeed", moveDir.magnitude);
+            if ( Mathf.Abs(moveDir.x) != 0 && Mathf.Abs(moveDir.z) != 0 )
+            {
+
+                return;
+            }
+            else if ( !moveOn && moveDir.magnitude > 0 )
+            {
+                moveOn = true;
+
+                StartCoroutine(MoveRoutine(moveDir));
+
+                transform.forward = moveDir;
+
+            }
 
         }
-        else if(!moveOn && grabOn &&moveDir.magnitude > 0)
+        else if ( !moveOn && grabOn && moveDir.magnitude > 0 )
         {
-            if ( grabHit.collider != null)
+            if ( grabHit.collider != null )
             {
                 Vector3 grabDir = ( grabHit.collider.gameObject.transform.position - transform.position ).normalized;
                 moveOn = true;
-                if ( grabDir.x > 0.9f &&moveDir.x < 0f)
+                if ( grabDir.x > 0.9f && moveDir.x < 0f )
                 {
-                    
+
                     bool X = true;
-                    StartCoroutine(PullRoutine(grabDir,X));
+                    StartCoroutine(PullRoutine(grabDir, X));
                     Debug.Log($"{grabDir}떙겨x");
                 }
-                else if(grabDir.x < -0.9f &&moveDir.x > 0f)
+                else if ( grabDir.x < -0.9f && moveDir.x > 0f )
                 {
                     bool X = true;
                     StartCoroutine(PullRoutine(grabDir, X));
                     Debug.Log($"{grabDir}떙겨x");
                 }
-                else if (grabDir.z > 0.9f &&moveDir.z <0f)
+                else if ( grabDir.z > 0.9f && moveDir.z < 0f )
                 {
                     bool X = false;
                     StartCoroutine(PullRoutine(grabDir, X));
                     Debug.Log($"{grabDir}떙겨z");
                 }
-                else if (grabDir.z < -0.9f && moveDir.z > 0f )
+                else if ( grabDir.z < -0.9f && moveDir.z > 0f )
                 {
                     bool X = false;
                     StartCoroutine(PullRoutine(grabDir, X));
@@ -135,28 +138,38 @@ using UnityEngine.Windows;
                 else
                 {
                     Debug.Log("상정외 ");
-                    moveOn = false; 
+                    moveOn = false;
                 }
-               
+
             }
-            
-            
+
+
         }
-        
-       
+
+
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        if(obstacle != null &&otherObs.point!=null)
-        Gizmos.DrawLine(obstacle.position, otherObs.point);
+        if ( obstacle != null && otherObs.point != null )
+            // Gizmos.DrawLine(obstacle.position, otherObs.point);
+            if ( grabHit.collider != null )
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawLine(grabHit.collider.transform.position, new Vector3(0, 1, 0));
+
+            }
     }
-    private IEnumerator PullRoutine(Vector3 pullDir,bool X)
+    private IEnumerator PullRoutine( Vector3 pullDir, bool X )
     {
+
+
+        Vector3 startPos = transform.position;
         Vector3 targetPos = pullDir;
+        Vector3 grabStartPos = grabHit.collider.transform.position;
         Vector3 grabTargetPos = pullDir;
-        if ( X ) 
+        if ( X )
         {
             targetPos = transform.position + new Vector3(-pullDir.x, 0, 0) * moveDistance;
             grabTargetPos = grabHit.collider.transform.position + new Vector3(-pullDir.x, 0, 0) * moveDistance;
@@ -166,50 +179,69 @@ using UnityEngine.Windows;
             targetPos = transform.position + new Vector3(0, 0, -pullDir.z) * moveDistance;
             grabTargetPos = grabHit.collider.transform.position + new Vector3(0, 0, -pullDir.z) * moveDistance;
         }
-        Vector3 startPos = transform.position;
-        Vector3 grabStartPos = grabHit.collider.transform.position;  
+
+
+        List<RaycastHit> hitArray = new List<RaycastHit>(Physics.RaycastAll(grabHit.collider.transform.position, grabHit.collider.transform.up, 10f));
+
+        hitArray.Add(grabHit);
+
+        foreach ( RaycastHit hit in hitArray )
+        {
+            hit.collider.gameObject.transform.SetParent(grabHit.collider.transform, true);
+            hit.rigidbody.isKinematic = true;
+        }
+
+        Debug.Log(hitArray.Count);
         float time = 0;
         float targetTime = 2;
-        while(time <2 )
+        while ( time < 2 )
         {
-            
+
             time += Time.deltaTime;
-            rb.MovePosition(Vector3.Lerp(startPos, targetPos, time/targetTime));
-            grabHit.rigidbody.MovePosition(Vector3.Lerp(grabStartPos, grabTargetPos, time/ targetTime));
-            if(time >= 2 )
-            moveOn = false;
-            
-           
-                yield return null;
+            rb.MovePosition(Vector3.Lerp(startPos, targetPos, time / targetTime));
+            grabHit.rigidbody.MovePosition(Vector3.Lerp(grabStartPos, grabTargetPos, time / targetTime));
+            yield return null;
+
+
+            if ( time >= 2 )
+            {
+                foreach ( RaycastHit hit in hitArray )
+                {
+                    hit.collider.gameObject.transform.SetParent(null, true);
+                    hit.rigidbody.isKinematic = false;
+                }
+                moveOn = false;
+            }
+
+
+
+
         }
-        
 
-
-        Debug.Log("무브온펄스");
     }
-    private IEnumerator MoveRoutine(Vector3 moveDirValue)
-        {
-           Vector3 targetPos = transform.position + moveDirValue * moveDistance;
+    private IEnumerator MoveRoutine( Vector3 moveDirValue )
+    {
+        Vector3 targetPos = transform.position + moveDirValue * moveDistance;
         Vector3 startPos = transform.position;
         RaycastHit hit;
         Vector3 PreMoveDir = moveDir;
-        if (Physics.Raycast(transform.position, moveDirValue, out hit, 1f) )
+        if ( Physics.Raycast(transform.position, moveDirValue, out hit, 1f) )
         {
             if ( hit.collider.CompareTag("Obstacle") )
             {
-               
+
                 obstacle = hit.transform;
                 Vector3 obsStartPos = obstacle.position;
                 Vector3 obsTargetPos = obstacle.position + moveDirValue * moveDistance;
-                Rigidbody obsRb = obstacle.GetComponent<Rigidbody>();
+
                 float time = 0;
 
                 if ( Physics.Raycast(obstacle.position, moveDirValue, out otherObs, 1f) )
                 {
                     if ( otherObs.collider.CompareTag("Obstacle") )
                     {
-                   
-                    moveOn = false;
+
+                        moveOn = false;
                         if ( moveDir.magnitude < 1 || PreMoveDir != moveDir )
                         {
                             animator.SetBool("Push", false);
@@ -218,12 +250,21 @@ using UnityEngine.Windows;
                     }
                     else
                     {
+                        List<RaycastHit> pushHitArray = new List<RaycastHit>(Physics.RaycastAll(hit.collider.transform.position, hit.collider.transform.up, 10f));
+
+                        pushHitArray.Add(hit);
+
+                        foreach ( RaycastHit hits in pushHitArray )
+                        {
+                            hits.collider.gameObject.transform.SetParent(hit.collider.transform, true);
+                            hits.rigidbody.isKinematic = true;
+                        }
                         while ( time < 2 )
                         {
                             animator.SetBool("Push", true);
                             time += Time.deltaTime * moveSpeed;
-                            rb.MovePosition(Vector3.Lerp(startPos, targetPos, time/2));
-                            obsRb.MovePosition(Vector3.Lerp(obsStartPos, obsTargetPos, time/2));
+                            rb.MovePosition(Vector3.Lerp(startPos, targetPos, time / 2));
+                            hit.rigidbody.MovePosition(Vector3.Lerp(obsStartPos, obsTargetPos, time / 2));
                             yield return null;
                         }
                         moveOn = false;
@@ -231,29 +272,48 @@ using UnityEngine.Windows;
                         {
                             animator.SetBool("Push", false);
                         }
+                        foreach ( RaycastHit hits in pushHitArray )
+                        {
+                            hits.collider.gameObject.transform.SetParent(null, true);
+                            hits.rigidbody.isKinematic = false;
+                        }
 
                         Debug.Log("장애물도 같이 무브");
                     }
                 }
                 else
                 {
+                    List<RaycastHit> pushHitArray = new List<RaycastHit>(Physics.RaycastAll(hit.collider.transform.position, hit.collider.transform.up, 10f));
+
+                    pushHitArray.Add(hit);
+
+                    foreach ( RaycastHit hits in pushHitArray )
+                    {
+                        hits.collider.gameObject.transform.SetParent(hit.collider.transform, true);
+                        hits.rigidbody.isKinematic = true;
+                    }
                     while ( time < 2 )
                     {
                         animator.SetBool("Push", true);
                         time += Time.deltaTime * moveSpeed;
                         rb.MovePosition(Vector3.Lerp(startPos, targetPos, time / 2));
-                        obsRb.MovePosition(Vector3.Lerp(obsStartPos, obsTargetPos, time / 2));
+                        hit.rigidbody.MovePosition(Vector3.Lerp(obsStartPos, obsTargetPos, time / 2));
                         yield return null;
-                        
-                            
-                          
+
+
+
                     }
                     moveOn = false;
-                    if ( moveDir.magnitude < 1 || PreMoveDir!=moveDir)
+                    if ( moveDir.magnitude < 1 || PreMoveDir != moveDir )
                     {
                         animator.SetBool("Push", false);
                     }
-                    
+                    foreach ( RaycastHit hits in pushHitArray )
+                    {
+                        hits.collider.gameObject.transform.SetParent(null, true);
+                        hits.rigidbody.isKinematic = false;
+                    }
+
                     Debug.Log("장애물도 같이 무브");
                 }
 
@@ -265,15 +325,15 @@ using UnityEngine.Windows;
         }
         else
         {
-                float time = 0;
-                while ( time < 1 )
-                {
+            float time = 0;
+            while ( time < 1 )
+            {
 
-                    time += Time.deltaTime * moveSpeed;
-                    rb.MovePosition(Vector3.Lerp(startPos, targetPos, time));
-                    yield return null;
-                }
+                time += Time.deltaTime * moveSpeed;
+                rb.MovePosition(Vector3.Lerp(startPos, targetPos, time));
                 yield return null;
+            }
+            yield return null;
             moveOn = false;
             if ( moveDir.magnitude < 1 || PreMoveDir != moveDir )
             {
@@ -282,11 +342,11 @@ using UnityEngine.Windows;
 
             Debug.Log("무브");
         }
-               
-            
-            
-     
-        }
 
-   
+
+
+
     }
+
+
+}
